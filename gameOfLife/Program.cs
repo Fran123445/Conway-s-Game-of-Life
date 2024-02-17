256afcfc8120b5014558b17d0a9aca817816e96c
Franco <fralstazzone@gmail.com>
2024-02-17 01:32:56 -0300

    Adds a function to print the array to the console

--- gameOfLife/Program.cs
+++ gameOfLife/Program.cs
@@ -4,9 +4,29 @@ namespace gameOfLife
 {
     class Program
     {
+
+        static void showCells(int[,] cells)
+        {
+            for (int i = 0; i < cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < cells.GetLength(1); j++)
+                {
+                    if (cells[i,j] != 0)
+                    {
+                        Console.Write("*");
+                    } else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
         static void Main()
         {
-            int[,] cells = new int[10, 10];
+            int[,] cells = new int[11, 11];
         }
     }
 }
