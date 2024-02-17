cf7c885549dfc08965646bf6bb8b68216b0508c4
Franco <fralstazzone@gmail.com>
2024-02-17 03:26:49 -0300

    Adds a method to take a step

--- gameOfLife/Program.cs
+++ gameOfLife/Program.cs
@@ -24,9 +24,69 @@ namespace gameOfLife
             }
         }
 
+
+        static int countCells(int[,] cells, int[] cellPosition)
+        {
+            int count = 0;
+            int[] steps = { -1, 0, 1 };
+            
+            foreach (var row in steps)
+            {
+                foreach (var column in steps)
+                {
+                    try
+                    {
+                        count += cells[cellPosition[0] + row, cellPosition[1] + column];
+                    } catch (IndexOutOfRangeException) {
+                        continue;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        static void step(int[,] cells)
+        {
+            int[,] originalState = new int[cells.GetLength(0), cells.GetLength(1)];
+            int count;
+            Array.Copy(cells, originalState, cells.Length);
+
+            for (int i = 0;i < originalState.GetLength(0);i++)
+            {
+                for (int j = 0;j < originalState.GetLength(1);j++)
+                {
+                    count = countCells(originalState, new int[] { i, j });
+
+                    if (originalState[i,j] != 0)
+                    {
+                        if (count-1 < 2 || count-1 > 3) // so it doesn't count itself
+                        {
+                            cells[i, j] = 0;
+                        }
+                    } else
+                    {
+                        if (count == 3)
+                        {
+                            cells[i, j] = 1;
+                        }
+                    }
+                }
+            }
+
+        }
+
         static void Main()
         {
-            int[,] cells = new int[11, 11];
+            int[,] cells = new int[10, 10];
+
+            while (true)
+            {
+                showCells(cells);
+                step(cells);
+                System.Threading.Thread.Sleep(100);
+            }
+
         }
     }
 }
